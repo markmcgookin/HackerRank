9c60c8af1169ba72fa1ed9bf52e9f2d55ae43133
Mark McGookin <mark@brightpay.co.uk>
2019-05-20 09:02:07 +0100

    Addeed 3 and 4

---
+++ 3-compare-triplets/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the compareTriplets function below.
+    static List<int> compareTriplets(List<int> a, List<int> b)
+    {
+        int a_score = 0;
+        int b_score = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            if (a[i] > b[i]) { a_score++; }
+            else if (b[i] > a[i]) { b_score++; }
+        }
+
+        return new List<int>() { a_score, b_score };
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        var a = new List<int>() { 1, 2, 3 };
+        //List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        var b = new List<int>() { 3, 2, 1 };
+        //List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+        List<int> result = compareTriplets(a, b);
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ 4-a-very-big-sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the aVeryBigSum function below.
+    static long aVeryBigSum(long[] ar)
+    {
+        return ar.Sum();
+    }
+
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        long[] ar = new List<long>() { 1000000001, 1000000002, 1000000003, 1000000004, 1000000005 }.ToArray();
+        //int arCount = Convert.ToInt32(Console.ReadLine());
+        //long[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt64(arTemp));
+
+        long result = aVeryBigSum(ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
