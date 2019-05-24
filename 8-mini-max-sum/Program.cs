7bd59c0573f339ab9c63e1167e1b1eaf2057b902
Mark McGookin <mark@brightpay.co.uk>
2019-05-24 09:17:14 +0100

    added 8 mini max

---
+++ 8-mini-max-sum/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr)
+    {
+        var minArr = new int[arr.Length];
+        Array.Sort(arr);
+        Array.Copy(arr, minArr, arr.Length);
+        Array.Reverse(arr);
+        long min = 0;
+        long max = 0;
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            max += arr[i];
+            min += minArr[i];
+        }
+
+        var textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        textWriter.WriteLine($"{min} {max}");
+        textWriter.Flush();
+        textWriter.Close();
+    }
+
+    static void Main(string[] args)
+    {
+        //int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+        miniMaxSum(arr);
+    }
+}
